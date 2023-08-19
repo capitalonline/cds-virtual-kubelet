9e61afada557955d6cb296e17e824e8483bb5e97
junqiang <junqiang.wang@qq.com>
2023-08-19 12:42:50 +0800

    fix bug

--- vendor/github.com/virtual-kubelet/virtual-kubelet/node/pod.go
+++ vendor/github.com/virtual-kubelet/virtual-kubelet/node/pod.go
@@ -272,15 +272,14 @@ func (pc *PodController) updatePodStatus(ctx context.Context, pod *corev1.Pod) e
 			return pkgerrors.Wrap(err, "error looking up pod")
 		}
 
-		if shouldSkipPodStatusUpdate(newPod) {
+		log.G(ctx).Debug("111pod status: %s, new pod status: %s, skip this", pod.Status.Phase, newPod.Status.Phase)
+		if pod.Status.Phase == newPod.Status.Phase {
+			log.G(ctx).Debug("222pod status: %s, new pod status: %s, skip this", pod.Status.Phase, newPod.Status.Phase)
 			return nil
 		}
 
-		if status.Phase == newPod.Status.Phase {
-			return nil
-		}
-
-		newPod.Status = pod.Status
+		newPod.Status = *status
+		log.G(ctx).Debug("update new pod status: %+v", newPod.Status)
 
 		if _, err := pc.client.Pods(pod.Namespace).UpdateStatus(newPod); err != nil {
 			span.SetStatus(err)
